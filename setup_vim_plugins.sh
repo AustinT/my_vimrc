8742b463d7b96aba910467fda2a1cb21b0eb0416
austin <tripp.aust@gmail.com>
2018-09-29 17:56:43 -0400

    Added scripts that install vim plugins

---
+++ setup_vim_plugins.sh
@@ -0,0 +1,7 @@
+# Pathogen
+apt-get install curl
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Supertab
+git clone https://github.com/ervandew/supertab.git ~/.vim/bundle
