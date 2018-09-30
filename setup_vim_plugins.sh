757c2e2de2490eb0383fd8214aaced1e58abfa32
Austin T <austin.james.tripp@gmail.com>
2018-09-29 22:40:39 -0400

    Fixed bug setting up supertab

--- setup_vim_plugins.sh
+++ setup_vim_plugins.sh
@@ -4,4 +4,7 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # Supertab
-git clone https://github.com/ervandew/supertab.git ~/.vim/bundle
+orig_dir=$(pwd)
+cd ~/.vim/bundle
+git clone https://github.com/ervandew/supertab.git
+cd $orig_dir
